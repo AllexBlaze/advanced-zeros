308c284d077eb6ca06428e9e625eb2cc5975397e
AllexBlaze <naum4ick@yandex.ru>
2018-09-29 23:36:52 +0300

    v1.0

--- src/index.js
+++ src/index.js
@@ -1,3 +1,46 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
-}
+
+
+  // Getting of prime multipliers and their quantity in the num
+  let getPrimeMultipliers = num => {
+    // Prime numbers checking
+    let isPrime = num => {
+      for (var i = 2; i < num; i++) if (num % i == 0) return false;
+      return num >= 2; 
+    };
+
+    let multiObj = {};
+    for (let i = 2, end = num, helpNum = num; i <= end; i++) {
+      if (isPrime(i)){
+        while(helpNum % i === 0){
+          helpNum = helpNum / i;
+          (multiObj[i]) ? multiObj[i]++ : multiObj[i] = 1 ;
+        };
+      };
+    };
+    return multiObj;
+  };
+
+  // Getting of the max prime number's power which the number can be divided
+  let getMaxPrimePow = (number, prime) => {
+    let ourNumber = number, ourPrime = prime, powCounter = 0;
+    while( ourNumber > 0){
+      ourNumber = ~~(ourNumber/ourPrime);
+      powCounter += ourNumber;
+    };
+    return powCounter;
+  };
+
+  let baseMultipliers = getPrimeMultipliers(base);
+  let result = [];
+
+
+  for (i in baseMultipliers) {
+    let numberMultipliers = {};
+    numberMultipliers[i] = getMaxPrimePow(number,i);
+
+    result.push( numberMultipliers[i] / baseMultipliers[i] );
+  };
+
+  return ~~Math.min.apply(null, result);
+};
